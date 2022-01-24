2d7cea25be11e26de98d7e98977939ad6bf5428e
Minjung Kim <sailor061@knou.ac.kr>
2022-01-24 21:50:55 +0900

    Initial commit

---
+++ src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava { // class name
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        //a statement which prints the argument passed to it
+    }
+}
---
+++ src/StringExample.java
@@ -0,0 +1,14 @@
+public class StringExample {
+    public static void main(String[] args) {
+        System.out.println("this is string");
+        System.out.println('s');
+
+        System.out.println("Hello this is \"java\"."); // \" name \"
+
+        System.out.println("Lorem ipsum dolor sit amet.\nConsectetur adipiscing elit.");
+        // \n = <br>
+        /*
+            multiline comment
+        */
+    }
+}

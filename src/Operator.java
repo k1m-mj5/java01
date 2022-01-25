aa80e1037d29314747382841e81abc4444b8720e
Minjung Kim <sailor061@knou.ac.kr>
2022-01-25 22:00:31 +0900

    operator

---
+++ src/Operator.java
@@ -0,0 +1,24 @@
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println(2 + 3);
+        System.out.println(5 - 2);
+        System.out.println(2 * 3);
+        System.out.println(6 / 3); // 2
+        System.out.println(7 % 3); // 1
+        System.out.println(7 / 2); // 3
+        System.out.println((2 - 4) / 2); // -1
+
+        System.out.println(1 + 1.2); // 2.2
+        System.out.println(7 / 2); // 3
+        System.out.println(7 / 2.0); // 3.5
+
+        System.out.println(2 < 3); // t
+        System.out.println(2 <= 3); // t
+        System.out.println(2 > 3);  // f
+        System.out.println(3 >= 3); // t
+        System.out.println(5 == 3); // f
+        System.out.println(5 == 5); // t
+        System.out.println(5 != 5); // f
+        System.out.println(3 != 5); // t
+    }
+}
---
+++ src/OperatorInt.java
@@ -0,0 +1,10 @@
+public class OperatorInt {
+    public static void main(String[] args) {
+
+        int number = 5;
+        // number = 6;
+        number = (int) 1.2; // 1
+        System.out.println(number);
+
+    }
+}
